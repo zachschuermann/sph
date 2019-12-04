3faee1b0199f15afedd83a2dc11e3c21fd2fcc44
Zach Schuermann <zachary.zvs@gmail.com>
2019-12-04 18:43:14 -0500

    add link, colors, etc.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,11 @@ module Main where
 import Lib
 import Graphics.UI.GLUT
 
+-- very helpful: https://wiki.haskell.org/OpenGLTutorial2
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
@@ -9,9 +14,33 @@ main = do
   displayCallback $= display
   mainLoop
 
-display :: DisplayCallback
-display = do
-  clear [ ColorBuffer ]
-  flush
+-- display :: DisplayCallback
+-- display = do
+--   clear [ ColorBuffer ]
+--   renderPrimitive Triangles $
+--      mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+--   flush
 
--- main = someFunc
+display = do
+  let color3f r g b = color $ Color3 r g (b :: GLfloat)
+      vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
+  clear [ColorBuffer]
+  renderPrimitive Quads $ do
+    color3f 1 0 0
+    vertex3f 0 0 0
+    vertex3f 0 0.2 0
+    vertex3f 0.2 0.2 0
+    vertex3f 0.2 0 0
+
+    color3f 0 1 0
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f 0.2 (-0.2) 0
+    vertex3f 0.2 0 0
+
+    color3f 0 0 1
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f (-0.2) (-0.2) 0
+    vertex3f (-0.2) 0 0
+  flush
