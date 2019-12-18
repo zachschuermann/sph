5096b258d8e927b2629040354f55c47eb3a4b5d1
Zach Schuermann <zachary.zvs@gmail.com>
2019-12-17 22:09:42 -0500

    plotter add

--- test/plot.py
+++ test/plot.py
@@ -14,10 +14,23 @@ def plotter(stats):
         plt.plot(x, y)
         plt.show()
 
+
+def get_stats(stats):
+    for core, stat in stats.items():
+        cleanstat = {int(k):v for k,v in stat.items()}
+        key_min = min(cleanstat.keys(), key=(lambda k: cleanstat[k][0]))
+        print("{}-core SPH: ".format(core), key_min, "chunks")
+        print("{}-core SPH: ".format(core), cleanstat[key_min])
+        print("{}-core SPH: ".format(core), 30/cleanstat[key_min][0]," speedup")
+        print()
+
+
 def main():
     with open("stat.json") as f:
         stats = json.load(f)
         plotter(stats)
+        get_stats(stats)
 
+       
 if __name__ == '__main__':
     sys.exit(main())
