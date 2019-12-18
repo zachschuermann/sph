6af8d432e6a358b67111f5b7f789e24f74348099
Zach Schuermann <zachary.zvs@gmail.com>
2019-12-18 12:51:33 -0500

    add plots

--- test/plot.py
+++ test/plot.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import json
 import sys
 
+### SERIAL ###
+# avg:  29.849999999999998
+# stddev:  0.4526035793053339
+
 def plotter(stats):
     for core, stat in stats.items():
         cleanstat = {int(k):v for k,v in stat.items()}
@@ -15,21 +19,63 @@ def plotter(stats):
         plt.show()
 
 
+def plot_totals(totals, keys):
+    tot_runtime = [29.85]
+    for core, tot in totals.items():
+        cleantot = {int(k):v for k,v in tot.items()}
+        plt.title("{}-core SPH".format(core))
+        ls = sorted(cleantot.items())
+        x, y = zip(*ls)
+        y = [i[0] for i in y]
+        plt.plot(x, y)
+        plt.show()
+        tot_runtime.append(cleantot[keys[core]][0])
+    return tot_runtime
+
 def get_stats(stats):
+    speedups = [1]
+    runtimes = [29.85]
+    keys = {}
     for core, stat in stats.items():
         cleanstat = {int(k):v for k,v in stat.items()}
         key_min = min(cleanstat.keys(), key=(lambda k: cleanstat[k][0]))
         print("{}-core SPH: ".format(core), key_min, "chunks")
         print("{}-core SPH: ".format(core), cleanstat[key_min])
-        print("{}-core SPH: ".format(core), 29.894/cleanstat[key_min][0]," speedup")
+        print("{}-core SPH: ".format(core), 29.85/cleanstat[key_min][0]," speedup")
+        print()
+        speedups.append(29.85/cleanstat[key_min][0])
+        runtimes.append(cleanstat[key_min][0])
+        keys[core] = key_min
+    return speedups, runtimes, keys
+
+def total_stats(stats):
+    for core, stat in stats.items():
+        cleanstat = {int(k):v for k,v in stat.items()}
+        key_min = min(cleanstat.keys(), key=(lambda k: cleanstat[k][0]))
+        print("{}-core SPH: ".format(core), key_min, "chunks")
+        print("{}-core SPH: ".format(core), cleanstat[key_min])
         print()
 
+def overall_plot(speedups, runtimes, tot_runtimes):
+    cores = range(1, 7)
+    # plot cores vs. speedups
+    plt.plot(cores, speedups)
+    plt.show()
+    plt.plot(cores, tot_runtimes)
+    plt.show()
+    plt.plot(cores, runtimes)
 
 def main():
     with open("stat3.json") as f:
         stats = json.load(f)
         plotter(stats)
-        get_stats(stats)
+        speedups, runtimes, keys = get_stats(stats)
+
+    with open("tots3.json") as f:
+        tots = json.load(f)
+        tot_runtime = plot_totals(tots, keys)
+
+    overall_plot(speedups, runtimes, tot_runtime)
 
        
 if __name__ == '__main__':
